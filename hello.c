ac0086ee3adf569b8ef6926755d3e6cebba2ccd9
hvallabhaneni2 <hvallabhaneni2.unl.edu>
2025-08-30 23:05:49 +0000

    Sat, Aug 30, 2025, 6:05 PM -05:00

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Harshi
+ * Date: 26/08/2025
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Harshi Vallabhaneni\n");
+    printf("Mehchanical Engineering\n");
+    return 0;
+}
